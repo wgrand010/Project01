a1808d85d34cc2bbdf2c5ec904550af4138aa8c4
wgrand <wgrand010@gmail.com>
2020-09-14 20:09:46 -0500

    initial commit

---
+++ Assets/PlayerCharaterAnimator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class PlayerCharaterAnimator : MonoBehaviour
+{
+    [SerializeField] ThirdPersonMovement _thirdPersonMovement = null;
+    const string IdleState = "Idle";
+    const string RunState = "Run";
+    const string JumpState = "Jumping";
+    const string FallState = "Falling";
+
+    Animator _animator = null;
+
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+
+    }
+
+    public void OnIdle()
+    {
+        _animator.CrossFadeInFixedTime(IdleState, .2f);
+    }
+
+
+    private void OnEnable()
+    {
+        _thirdPersonMovement.Idle += OnIdle;
+        _thirdPersonMovement.StartRunning += OnStartRunning;
+    }
+
+    private void OnDisable()
+    {
+        _thirdPersonMovement.Idle -= OnIdle;
+        _thirdPersonMovement.StartRunning -= OnStartRunning;
+    }
+
+    private void OnStartRunning()
+    {
+        _animator.CrossFadeInFixedTime(RunState, .2f);
+    }
+}
---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class ThirdPersonMovement : MonoBehaviour
+
+
+{
+    public event Action Idle = delegate { };
+    public event Action StartRunning = delegate { };
+   
+
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnsmoothvelocity;
+    bool _ismoving = false;
+
+
+    private void Start()
+    {
+        Idle?.Invoke();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, Vertical).normalized;
+
+        if (direction.magnitude >= 0.2f)
+        {
+            CheckIfStartedMoving();
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnsmoothvelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        else
+        {
+            CheckIfStoppedMoving();
+        }
+    }
+        private void CheckIfStartedMoving()
+        {
+            if(_ismoving == false)
+            {
+                StartRunning?.Invoke();
+                Debug.Log("Started Running");
+            }
+            _ismoving = true;
+        }
+
+        private void CheckIfStoppedMoving()
+        {
+            if (_ismoving == true)
+            {
+                Idle?.Invoke();
+                Debug.Log("Stopped");
+            }
+            _ismoving = false;
+        }
+    
+}
