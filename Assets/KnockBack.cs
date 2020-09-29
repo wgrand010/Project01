c395a8f2c4e51f1238b7e731d2de8abe0fc597fa
wgrand <wgrand010@gmail.com>
2020-09-29 00:10:06 -0500

    final commit

---
+++ Assets/KnockBack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    [SerializeField ] private float knockbackstrength;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        CharacterController cc = hit.collider.GetComponent<CharacterController>();
+
+        if (cc != null )
+        {
+            Vector3 direction = hit.transform.position - transform.position;
+            direction.y = 0;
+           
+
+            cc.Move(direction.normalized * knockbackstrength);
+        }
+    }
+}
--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ThirdPersonMovement : MonoBehaviour
 
@@ -12,16 +14,24 @@ public class ThirdPersonMovement : MonoBehaviour
     public event Action StartRunning = delegate { };
     public event Action StartJumping = delegate { };
     [SerializeField] AudioClip AbilitySound;
-
+    [SerializeField] AudioClip HurtSound;
+    AudioSource audioSource;
 
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    
     public float gravity = -9.81f;
     Vector3 velocity;
     bool isGrounded;
     public float jumpHeight = 3;
+    private const float walkSpeed = 5.0f;
+    private const float runSpeed = 10.0f;
+    public float speed = walkSpeed;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public healthbar _healthbar;
 
     public Transform groundCheck;
     public float groundDistance = 0;
@@ -35,9 +45,19 @@ public class ThirdPersonMovement : MonoBehaviour
 
     private void Start()
     {
+        currentHealth = maxHealth;
+        _healthbar.SetMaxHealth(maxHealth);
         Idle?.Invoke();
     }
-    
+
+    private void OnControllerColliderHit(ControllerColliderHit other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            TakeDamage(1);
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -47,6 +67,20 @@ public class ThirdPersonMovement : MonoBehaviour
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -5f;
+        }
+
+         
+
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(20);
+            Debug.Log("taking damage");
+        }
+
+
+        if (currentHealth == 0)
+        {
+            SceneManager.LoadScene("SampleScene");
         }
 
         //gravity
@@ -83,8 +117,23 @@ public class ThirdPersonMovement : MonoBehaviour
         {
             Jumping();
         }
+
+       if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+       else
+        {
+            speed = walkSpeed;
+        }
+       
+       
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            if (AbilitySound != null)
+            {
+                AudioHelper.PlayClip2D(AbilitySound, 1f);
+            }
             jumpHeight = 10;
             StartCoroutine(levelup());
 
@@ -141,6 +190,17 @@ public class ThirdPersonMovement : MonoBehaviour
             isGrounded = false;
         }
         isGrounded = false;
+    }
+
+    [Obsolete]
+    void TakeDamage(int damage)
+    {
+        if (AbilitySound != null && AbilitySound.isReadyToPlay)
+        {
+            AudioHelper.PlayClip2D(HurtSound, 1f);
+        }
+        currentHealth -= damage;
+        _healthbar.SetHealth(currentHealth);
     }
 
 
